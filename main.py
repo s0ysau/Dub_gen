b235f9f992fb1d05d2426368e3abbedfc56d3049
Soysau <105724406+s0ysau@users.noreply.github.com>
2022-05-31 15:16:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+# what is the length of pw
+# if special characters are needed
+
+lenght_of_pw = input ("How long do you want the desired pw? ")
+
+special_char = input ("Are special characters needed? ")
+
